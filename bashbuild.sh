84843c0f396a7ab1e4850192ca07318988d0ee76
k0t0baza <39706290+k0t0baza@users.noreply.github.com>
2024-05-03 13:34:11 +0300

    bashbuild modified

--- bashbuild.sh
+++ bashbuild.sh
@@ -8,6 +8,11 @@ remote_main_commit=$(git rev-parse origin/main)
 # Сравниваем два коммита
 if [ "$head_commit" == "$remote_main_commit" ]; then
     echo "Коммиты совпадают"
+    sudo docker stop nodejs-container
+    sudo docker container rm nodejs-container
+    sudo docker image rm nodejs-container-image
+    sudo docker build . -t nodejs-container-image
+    sudo docker run -p 8080:3000 -d --name nodejs-container nodejs-container-image
 else
     git pull
     sudo docker stop nodejs-container
